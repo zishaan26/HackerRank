d980a4e00221339d5fea816c4c447cbd0e9f7e80
zishaan26 <zishaan.mystyle@gmail.com>
2020-08-23 02:45:46 +0530

    Print Function

---
+++ printFunction.py
@@ -0,0 +1,11 @@
+from __future__ import print_function
+
+if __name__ == '__main__':
+    n = 3
+    result = ""
+    for number in range(n):
+        result = result + str(number+1)
+    
+    print(result)
+
+
